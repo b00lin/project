bc3add0468fc25f88a7019f53262208b69567656
b00lin <67520657+b00lin@users.noreply.github.com>
2024-03-25 07:53:43 +0000

    first commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,14 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+use App\Controllers\Pages;
+
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+// The second route does a GET request to the /pages and will map it to the /index method of the pages class.
+$routes->get('pages', [Pages::class, 'index']);
+// Third rule does a GET request using the segment placeholder. Passes to the view() method of the pages class.
+$routes->get('(:segment)', [Pages::class, 'view']);
