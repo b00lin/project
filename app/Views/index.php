2ee94370dcdc7ee8de8de63117c376bfb53f3bda
b00lin <67520657+b00lin@users.noreply.github.com>
2024-03-28 21:48:48 +0000

    index.php (landing page) commit

---
+++ app/Views/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex flex-column justify-content-center">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-xl-8">
+          <h1>Welcome to LegalSupportHub</h1>
+          <h2>The leading source for legal support since 2024!</h2>
+        </div>
+      </div>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services">
+      <div class="container">
+        <div class="section-title">
+          <h2>Why LSH?</h2>
+          <p>Here at LSH, we strive for excellence and always aim to be above the competition.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="icon-box">
+              <div class="icon"><i class='bx bx-run'></i></div>
+              <h4><a href="">Fast Support</a></h4>
+              <p>Here at LSH we pride ourselves for having some of the fastest response time compared to our competitors.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+            <div class="icon-box">
+              <div class="icon"><i class='bx bxs-brain'></i></div>
+              <h4><a href="">Knowledgeable</a></h4>
+              <p>All staff at LSH are knowledeable in their field, making it easier to help you with your issue!</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-like"></i></div>
+              <h4><a href="">Favourited</a></h4>
+              <p>All LSH users <i class="bx bxs-heart"></i> their experience, it shows as we were customers choice in 2024.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxs-chat"></i></div>
+              <h4><a href="">Customer Support</a></h4>
+              <p>Our customer support works around the clock to ensure that you get the quality experience when using LegalSupportHub.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-money"></i></div>
+              <h4><a href="">Charitable</a></h4>
+              <p>LegalSupportHub is charitable where possible, helping smaller business floruish and grow.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-trophy"></i></div>
+              <h4><a href="">Award Winning</a></h4>
+              <p>Award winning customer support, here to help you whenever you need.</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
+  </main><!-- End #main -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <!-- JS File -->
+  <script src="assets/js/main.js"></script>
+</body>
+</html>
