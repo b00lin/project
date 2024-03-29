dcb69c15293ea9a2694752bcdf6eb91f2d5609ca
b00lin <67520657+b00lin@users.noreply.github.com>
2024-03-29 01:18:45 +0000

    Updating footer.php

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,3 +1,18 @@
-    <em>&copy; 2022</em>
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <h3>LegalSupportHub</h3>
+      <p>The leading provider in free legal guidance</p>
+      <div class="copyright">
+        Copyright &copy; LegalSupportHub LSH 2024 - 2024
+        <br>
+        NOTE: This is a project for a University project
+        </br>
+        <br>
+        Legal Advice CANNOT be sought from this website.
+        </br>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
 </body>
 </html>
