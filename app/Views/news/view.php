46ebb0dfa034d236ad657ecfeb70fd99c2ced866
b00lin <67520657+b00lin@users.noreply.github.com>
2024-05-14 21:48:28 +0100

    Fixing missing CSS.

--- app/Views/news/view.php
+++ app/Views/news/view.php
@@ -1,2 +1,10 @@
-<h2><?= esc($news['title']) ?></h2>
-<p><?= esc($news['body']) ?></p>
+<head>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style_news.css'); ?>">
+</head>
+<body>
+<div class="news">
+    <h2><?= esc($news['title']) ?></h2>
+    <p><?= esc($news['body']) ?></p>
+    <a href="<?= base_url('news') ?>" class="back-button">Back to Articles</a>
+</div>
+</body>
