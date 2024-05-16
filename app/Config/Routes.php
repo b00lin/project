90f6f88a6c61f6c9ad1a6957848b28e6563a4ab4
b00lin <67520657+b00lin@users.noreply.github.com>
2024-05-16 17:18:26 +0100

    Added routes for success and delete

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,8 +13,10 @@ $routes->get('news', [News::class, 'index']);
 $routes->get('news/new', [News::class, 'new']); 
 $routes->post('news/new', [News::class, 'new']); 
 $routes->post('news', [News::class, 'create']); 
+$routes->get('/news/success', 'News::success');
 
 $routes->get('news/(:segment)', [News::class, 'show']); 
+$routes->post('news/delete', 'News::delete');
 
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
