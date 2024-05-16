27115da87cbb07501d09a669a9561316a612a6a3
b00lin <67520657+b00lin@users.noreply.github.com>
2024-05-16 17:19:39 +0100

    Created success.php page instead of using prettyprint (json)

--- app/Views/news/success.php
+++ app/Views/news/success.php
@@ -1,1 +1,12 @@
-<p>News Item successfully created!</p>
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <div class="container">
+        <div class="success-message">
+            <h1 class="display-4">Success!</h1>
+            <p class="lead">Your news item has been added successfully.</p>
+            <a href="/news" class="btn btn-primary">Return to News</a>
+        </div>
+    </div>
+</body>
+</html>
